c74294b23e4e37c5def1075837f4c333a82731df
KushalJohari <kushaljohari28@gmail.com>
2025-08-05 16:03:47 +0530

    commit

--- Loaders.py
+++ Loaders.py
@@ -12,9 +12,14 @@ load_dotenv()
 llm = ChatGroq(model="llama3-8b-8192", api_key=	os.getenv("GROQ_API_KEY"))
 
 prompt = PromptTemplate(template=f"""
-You are an intelligent assistant that answers user questions accurately and clearly.
+You are a multilingual assistant. The user may ask questions in any language.
+The content you receive (context) might be in a different language (e.g., Hindi, Tamil, French).
+Your job is to **always answer in the language of the question**, even if the context is written in another language.
 
-Always respond in the **same language** the question is asked in.
+If the question is in English, and the context is in Hindi, **translate the context and answer in English.**
+Always prioritize the question language over the context language.
+                        
+example: user asks in hindi "इसमें क्या लिखा है" you will read the document user uploaded and then tell about the document in summary.
 
 Context:    
 {{context}}
@@ -37,14 +42,14 @@ def pdfloader(pdf):
     return pdf_document
 
 def qa_chain_web(documents):
-    embeddings = HuggingFaceEmbeddings(model_name= "xlm-roberta-base")
+    embeddings = HuggingFaceEmbeddings(model_name= "BAAI/bge-m3")
     textsplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     chunks = textsplitter.split_documents(documents)
     vector_store = FAISS.from_documents(chunks, embedding=embeddings)
     return RetrievalQA.from_chain_type(llm=llm, retriever=vector_store.as_retriever(), chain_type_kwargs={'prompt':prompt})
 
 def qa_chain_pdf(pdf_documents):
-    embeddings = HuggingFaceEmbeddings(model_name= "xlm-roberta-base")
+    embeddings = HuggingFaceEmbeddings(model_name= "BAAI/bge-m3")
     textsplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     chunks = textsplitter.split_documents(pdf_documents)
     vector_store = FAISS.from_documents(chunks, embedding=embeddings)
